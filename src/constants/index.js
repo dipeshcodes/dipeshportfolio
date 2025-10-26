e075064fbfea7ea1ddee425ecc2758310ff5cb0e
Dipesh <yadavdipesh2000@gmail.com>
2025-10-26 13:58:24 +0530

    updaate the projects section

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,49 +2,43 @@ export const myProjects = [
   {
     id: 1,
 
-    title: "3D Developer Portfolio",
+    title: "3D Reconstruction from 2D Images",
     description:
-      "A modern, animated 3D developer portfolio built with React, Three.js, TailwindCSS, and motion effects — designed to help you stand out and showcase your skills creatively.",
+      "A deep learning-based computer vision project that reconstructs realistic 3D structures from single 2D images using neural implicit representations — advancing visual perception for applications in VR, AR, and robotics.",
     subDescription: [
-      "Integrated 3D visuals using React Three Fiber and Drei to create a unique and immersive user experience.",
-      "Implemented smooth transitions and scroll-based animations leveraging Framer Motion for a dynamic feel.",
-      "Developed a clean, responsive UI with a utility-first approach using TailwindCSS.",
-      "Enhanced the aesthetic appeal and interactivity with custom components from Aceternity UI and Magic UI.",
-      "Set up a working contact form using EmailJS for direct user communication.",
-      "Utilized Vite for a fast development and build process.",
+      "Developed a machine learning pipeline to recover 3D geometry and appearance from monocular 2D images.",
+      "Proposed a novel two-stage learning curriculum to balance reconstruction losses, enhancing alignment between 2D and 3D supervision.",
+      "Utilized single-view neural implicit shape representations for improved realism in reconstructing textured 3D objects.",
+      "Implemented model training on paired 2D and 3D datasets, optimizing hyperparameters for high-quality reconstruction.",
+      "Conducted extensive experiments and analyses demonstrating superior performance over existing state-of-the-art methods.",
     ],
-    href: "https://github.com/Ali-Sanati/Portfolio.git",
+    href: "",
     logo: "",
-    image: "/assets/projects/portfolio.jpg",
+    image: "/assets/projects/3d_reconstruction.jpg",
     tags: [
       {
         id: 1,
-        name: "React",
-        path: "/assets/logos/react.svg",
-      },
-      {
-        id: 2,
-        name: "Three.js",
-        path: "/assets/logos/threejs.svg",
-      },
-      {
-        id: 3,
-        name: "TailwindCSS",
-        path: "/assets/logos/tailwindcss.svg",
-      },
-      {
-        id: 4,
-        name: "Framer Motion",
+        name: "Python",
         path: "",
       },
       {
-        id: 5,
-        name: "Vite",
-        path: "/assets/logos/vite.svg",
+        id: 2,
+        name: "Deep Learning",
+        path: "",
+      },
+      {
+        id: 3,
+        name: "CV2",
+        path: "",
+      },
+      {
+        id: 4,
+        name: "Neural Networks",
+        path: "",
       },
     ],
   },
-    {
+  {
     id: 2,
 
     title: "3D Developer Portfolio",
@@ -58,7 +52,7 @@ export const myProjects = [
       "Set up a working contact form using EmailJS for direct user communication.",
       "Utilized Vite for a fast development and build process.",
     ],
-    href: "https://github.com/Ali-Sanati/Portfolio.git",
+    href: "https://github.com/dipeshcodes/dipeshportfolio",
     logo: "",
     image: "/assets/projects/portfolio.jpg",
     tags: [
@@ -79,291 +73,87 @@ export const myProjects = [
       },
       {
         id: 4,
-        name: "Framer Motion",
-        path: "",
-      },
-      {
-        id: 5,
         name: "Vite",
         path: "/assets/logos/vite.svg",
       },
     ],
-  },,
-    {
+  },
+  {
     id: 3,
 
-    title: "3D Developer Portfolio",
+    title: "Digit Recognition Program",
     description:
-      "A modern, animated 3D developer portfolio built with React, Three.js, TailwindCSS, and motion effects — designed to help you stand out and showcase your skills creatively.",
+      "A Python-based deep learning project that accurately identifies handwritten digits, demonstrating strong programming and machine learning skills.",
     subDescription: [
-      "Integrated 3D visuals using React Three Fiber and Drei to create a unique and immersive user experience.",
-      "Implemented smooth transitions and scroll-based animations leveraging Framer Motion for a dynamic feel.",
-      "Developed a clean, responsive UI with a utility-first approach using TailwindCSS.",
-      "Enhanced the aesthetic appeal and interactivity with custom components from Aceternity UI and Magic UI.",
-      "Set up a working contact form using EmailJS for direct user communication.",
-      "Utilized Vite for a fast development and build process.",
-    ],
-    href: "https://github.com/Ali-Sanati/Portfolio.git",
-    logo: "",
-    image: "/assets/projects/portfolio.jpg",
-    tags: [
-      {
-        id: 1,
-        name: "React",
-        path: "/assets/logos/react.svg",
-      },
-      {
-        id: 2,
-        name: "Three.js",
-        path: "/assets/logos/threejs.svg",
-      },
-      {
-        id: 3,
-        name: "TailwindCSS",
-        path: "/assets/logos/tailwindcss.svg",
-      },
-      {
-        id: 4,
-        name: "Framer Motion",
-        path: "",
-      },
-      {
-        id: 5,
-        name: "Vite",
-        path: "/assets/logos/vite.svg",
-      },
-    ],
-  },
-    {
-    id: 4,
-
-    title: "3D Developer Portfolio",
-    description:
-      "A modern, animated 3D developer portfolio built with React, Three.js, TailwindCSS, and motion effects — designed to help you stand out and showcase your skills creatively.",
-    subDescription: [
-      "Integrated 3D visuals using React Three Fiber and Drei to create a unique and immersive user experience.",
-      "Implemented smooth transitions and scroll-based animations leveraging Framer Motion for a dynamic feel.",
-      "Developed a clean, responsive UI with a utility-first approach using TailwindCSS.",
-      "Enhanced the aesthetic appeal and interactivity with custom components from Aceternity UI and Magic UI.",
-      "Set up a working contact form using EmailJS for direct user communication.",
-      "Utilized Vite for a fast development and build process.",
-    ],
-    href: "https://github.com/Ali-Sanati/Portfolio.git",
-    logo: "",
-    image: "/assets/projects/portfolio.jpg",
-    tags: [
-      {
-        id: 1,
-        name: "React",
-        path: "/assets/logos/react.svg",
-      },
-      {
-        id: 2,
-        name: "Three.js",
-        path: "/assets/logos/threejs.svg",
-      },
-      {
-        id: 3,
-        name: "TailwindCSS",
-        path: "/assets/logos/tailwindcss.svg",
-      },
-      {
-        id: 4,
-        name: "Framer Motion",
-        path: "",
-      },
-      {
-        id: 5,
-        name: "Vite",
-        path: "/assets/logos/vite.svg",
-      },
-    ],
-  },
-  // {
-  //   id: 2,
-  //   title: "Authentication & Authorization System",
-  //   description:
-  //     "A secure authentication and authorization system using Auth0 for seamless user management.",
-  //   subDescription: [
-  //     "Integrated Auth0 for authentication, supporting OAuth, JWT, and multi-factor authentication.",
-  //     "Implemented role-based access control (RBAC) for fine-grained user permissions.",
-  //     "Developed a React-based frontend with Tailwind CSS for a sleek user experience.",
-  //     "Connected to a secure SQLite database for user data storage.",
-  //   ],
-  //   href: "",
-  //   logo: "",
-  //   image: "/assets/projects/auth-system.jpg",
-  //   tags: [
-  //     {
-  //       id: 1,
-  //       name: "Auth0",
-  //       path: "/assets/logos/auth0.svg",
-  //     },
-  //     {
-  //       id: 2,
-  //       name: "React",
-  //       path: "/assets/logos/react.svg",
-  //     },
-  //     {
-  //       id: 3,
-  //       name: "SQLite",
-  //       path: "/assets/logos/sqlite.svg",
-  //     },
-  //     {
-  //       id: 4,
-  //       name: "TailwindCSS",
-  //       path: "/assets/logos/tailwindcss.svg",
-  //     },
-  //   ],
-  // },
-  // {
-  //   id: 3,
-  //   title: "Blazor Web App",
-  //   description:
-  //     "A modern, interactive web application built with Blazor WebAssembly and .NET Core.",
-  //   subDescription: [
-  //     "Developed a fully interactive Single Page Application (SPA) using Blazor WebAssembly.",
-  //     "Implemented API interactions using .NET Core for a robust backend.",
-  //     "Designed responsive UI components with Tailwind CSS for an enhanced UX.",
-  //     "Integrated SQLite for efficient client-side database storage.",
-  //   ],
-  //   href: "",
-  //   logo: "",
-  //   image: "/assets/projects/blazor-app.jpg",
-  //   tags: [
-  //     {
-  //       id: 1,
-  //       name: "Blazor",
-  //       path: "/assets/logos/blazor.svg",
-  //     },
-  //     {
-  //       id: 2,
-  //       name: ".NET Core",
-  //       path: "/assets/logos/dotnetcore.svg",
-  //     },
-  //     {
-  //       id: 3,
-  //       name: "SQLite",
-  //       path: "/assets/logos/sqlite.svg",
-  //     },
-  //     {
-  //       id: 4,
-  //       name: "TailwindCSS",
-  //       path: "/assets/logos/tailwindcss.svg",
-  //     },
-  //   ],
-  // },
-  // {
-  //   id: 4,
-  //   title: "C++ Game Engine",
-  //   description:
-  //     "A lightweight C++ game engine designed for 2D and 3D game development.",
-  //   subDescription: [
-  //     "Built a powerful rendering engine using OpenGL and C++.",
-  //     "Developed a physics engine with collision detection and particle effects.",
-  //     "Implemented a scripting system for easy game customization.",
-  //     "Optimized performance with multi-threading and efficient memory management.",
-  //   ],
-  //   href: "",
-  //   logo: "",
-  //   image: "/assets/projects/game-engine.jpg",
-  //   tags: [
-  //     {
-  //       id: 1,
-  //       name: "C++",
-  //       path: "/assets/logos/cplusplus.svg",
-  //     },
-  //     {
-  //       id: 2,
-  //       name: "C#",
-  //       path: "/assets/logos/csharp.svg",
-  //     },
-  //     {
-  //       id: 3,
-  //       name: "Git",
-  //       path: "/assets/logos/git.svg",
-  //     },
-  //     {
-  //       id: 4,
-  //       name: "Microsoft",
-  //       path: "/assets/logos/microsoft.svg",
-  //     },
-  //   ],
-  // },
-  {
-    id: 5,
-    title: "WordPress Custom Theme",
-    description:
-      "A fully customizable WordPress theme optimized for performance and SEO.",
-    subDescription: [
-      "Developed a responsive WordPress theme using HTML5, CSS3, and JavaScript.",
-      "Integrated Tailwind CSS for modern styling and UI enhancements.",
-      "Optimized SEO and page speed using Vite.js for fast builds.",
-      "Implemented custom widgets and plugin compatibility for extended functionality.",
+      "Built a deep learning model for handwritten digit recognition using Python.",
+      "Trained and evaluated the model to achieve high accuracy in classifying digits from image data.",
+      "Demonstrated understanding of neural networks and computer vision fundamentals.",
+      "Implemented and tested the project in VSCode and PyCharm environments for optimized workflow.",
     ],
     href: "",
     logo: "",
-    image: "/assets/projects/portfolio.jpg",
+    image: "",
     tags: [
       {
         id: 1,
-        name: "WordPress",
-        path: "/assets/logos/wordpress.svg",
+        name: "Python",
+        path: "",
       },
       {
         id: 2,
-        name: "HTML5",
-        path: "/assets/logos/html5.svg",
+        name: "Deep Learning",
+        path: "",
       },
       {
         id: 3,
-        name: "CSS3",
-        path: "/assets/logos/css3.svg",
-      },
-      {
-        id: 4,
-        name: "Vite.js",
-        path: "/assets/logos/vitejs.svg",
+        name: "PyCharm",
+        path: "",
       },
     ],
   },
-  // {
-  //   id: 6,
-  //   title: "Online Learning Platform",
-  //   description:
-  //     "A web application that allows users to enroll in courses, watch video lectures, and take quizzes.",
-  //   subDescription: [
-  //     "Built using Blazor WebAssembly for a seamless SPA experience.",
-  //     "Implemented video streaming with Azure Media Services.",
-  //     "Added a quiz system with dynamic question generation and real-time grading.",
-  //     "Integrated Stripe API for secure payment processing.",
-  //   ],
-  //   href: "",
-  //   logo: "",
-  //   image: "/assets/projects/elearning.jpg",
-  //   tags: [
-  //     {
-  //       id: 1,
-  //       name: "Blazor",
-  //       path: "/assets/logos/blazor.svg",
-  //     },
-  //     {
-  //       id: 2,
-  //       name: "Azure",
-  //       path: "/assets/logos/azure.svg",
-  //     },
-  //     {
-  //       id: 3,
-  //       name: "Stripe",
-  //       path: "/assets/logos/stripe.svg",
-  //     },
-  //     {
-  //       id: 4,
-  //       name: "TailwindCSS",
-  //       path: "/assets/logos/tailwindcss.svg",
-  //     },
-  //   ],
-  // },
+  {
+    id: 4,
+
+    title: "E-Shopping Website",
+    description:
+      "A learning-based responsive e-commerce website built with React, TailwindCSS, and JavaScript — designed to deliver a smooth and engaging shopping experience.",
+    subDescription: [
+      "Developed a responsive and interactive e-commerce web interface using React and TailwindCSS.",
+      "Implemented dynamic product listings and a user-friendly layout for an intuitive shopping experience.",
+      "Utilized JavaScript for handling UI interactions and state-driven updates.",
+      "Applied modern front-end design principles for seamless responsiveness across devices.",
+    ],
+    href: "https://github.com/dipeshcodes/Amazon-clone-react",
+    logo: "",
+    image: "/assets/projects/e_commerce.jpg",
+    tags: [
+      {
+        id: 1,
+        name: "HTML",
+        path: "",
+      },
+      {
+        id: 2,
+        name: "CSS",
+        path: "",
+      },
+      {
+        id: 3,
+        name: "JavaScript",
+        path: "/assets/logos/javascript.svg",
+      },
+      {
+        id: 4,
+        name: "React",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 5,
+        name: "Tailwind",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
 ];
 
 export const mySocials = [
